2c1492954c9e5c1ada3a41dc5f9fe3d5bbbde96b
Nane Cerqueira <elaine.edc@hotmail.com>
2025-03-07 17:08:00 -0300

    construindo_sistema

---
+++ sistema_bancario.py
@@ -0,0 +1,22 @@
+
+def deposito():
+    valor = int(input('Qual o valor que deseja depositar?' ))
+    while True:
+        if valor 
+            
+        else:
+            print('Valor inválido, digite novamente')
+
+
+
+
+print(''' Menu:
+      1 - Depósito
+      2 - Saque
+      3 - Extrato
+    ''')
+opcao = (int(input('Digite a opção desejada: ')))
+
+while True:
+    if opcao == 1 :
+        print(deposito)
